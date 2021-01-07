0dd3013d383c0d34030f640b0cbc1965973b10f3
parkeradam <30392071+parkeradam@users.noreply.github.com>
2021-01-07 15:20:12 +0000

    Copyright updates and correlation Id additions.

--- test/component/uk/gov/hmrc/individualsbenefitsandcreditsapi/controllers/LiveWorkingTaxCreditControllerSpec.scala
+++ test/component/uk/gov/hmrc/individualsbenefitsandcreditsapi/controllers/LiveWorkingTaxCreditControllerSpec.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 HM Revenue & Customs
+ * Copyright 2021 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,7 +18,11 @@ package component.uk.gov.hmrc.individualsbenefitsandcreditsapi.controllers
 
 import java.util.UUID
 
-import component.uk.gov.hmrc.individualsbenefitsandcreditsapi.stubs.{AuthStub, IfStub, IndividualsMatchingApiStub}
+import component.uk.gov.hmrc.individualsbenefitsandcreditsapi.stubs.{
+  AuthStub,
+  IfStub,
+  IndividualsMatchingApiStub
+}
 import play.api.libs.json.Json
 import play.api.test.Helpers._
 import scalaj.http.Http
--- test/component/uk/gov/hmrc/individualsbenefitsandcreditsapi/stubs/BaseSpec.scala
+++ test/component/uk/gov/hmrc/individualsbenefitsandcreditsapi/stubs/BaseSpec.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 HM Revenue & Customs
+ * Copyright 2021 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -59,6 +59,7 @@ trait BaseSpec
   val authToken = "Bearer AUTH_TOKEN"
   val clientId = "CLIENT_ID"
   val acceptHeaderP1 = ACCEPT -> "application/vnd.hmrc.P1.0+json"
+  val correlationIdHeader = "CorrelationId" -> "188e9400-b636-4a3b-80ba-230a8c72b92a"
 
   def invokeEndpoint(endpoint: String) =
     Http(endpoint)
@@ -68,7 +69,10 @@ trait BaseSpec
 
   protected def requestHeaders(
       acceptHeader: (String, String) = acceptHeaderP1) =
-    Map(CONTENT_TYPE -> JSON, AUTHORIZATION -> authToken, acceptHeader)
+    Map(CONTENT_TYPE -> JSON,
+        AUTHORIZATION -> authToken,
+        acceptHeader,
+        correlationIdHeader)
 
   protected def errorResponse(message: String) =
     s"""{"code":"INVALID_REQUEST","message":"$message"}"""
