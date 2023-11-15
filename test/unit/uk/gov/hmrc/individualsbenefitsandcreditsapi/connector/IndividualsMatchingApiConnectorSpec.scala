8ecd30879f99b15e7bf7cdce426acda2e4724028
Karolina Zaborska <karolina.zaborska@digital.hmrc.gov.uk>
2023-11-15 14:47:20 +0000

    DLS-9131 Removal of ExecutionContext global

--- test/unit/uk/gov/hmrc/individualsbenefitsandcreditsapi/connector/IndividualsMatchingApiConnectorSpec.scala
+++ test/unit/uk/gov/hmrc/individualsbenefitsandcreditsapi/connector/IndividualsMatchingApiConnectorSpec.scala
@@ -26,6 +26,7 @@ import uk.gov.hmrc.http.{HeaderCarrier, HttpClient, UpstreamErrorResponse}
 import uk.gov.hmrc.individualsbenefitsandcreditsapi.connectors.IndividualsMatchingApiConnector
 import uk.gov.hmrc.individualsbenefitsandcreditsapi.domains.{MatchNotFoundException, MatchedCitizen}
 import unit.uk.gov.hmrc.individualsbenefitsandcreditsapi.utils.SpecBase
+import scala.concurrent.ExecutionContext.Implicits.global
 
 import java.util.UUID
 
