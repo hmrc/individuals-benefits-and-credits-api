ea60e3f20a9b133e043a8312d864e26a76b0a978
johno1985 <1640705+johno1985@users.noreply.github.com>
2024-10-02 14:43:05 +0100

    DLS-10898: Removed flexmark dependency

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,16 +2,13 @@ import play.sbt.PlayImport.ws
 import sbt.*
 
 object AppDependencies {
-
   val hmrc = "uk.gov.hmrc"
   val hmrcMongo = s"$hmrc.mongo"
   val playVersion = "play-30"
   val hmrcBootstrapVersion = "8.4.0"
   val hmrcMongoVersion = "1.7.0"
 
-
   val compile: Seq[ModuleID] = Seq(
-    ws,
     hmrc                %% s"bootstrap-backend-$playVersion"      % hmrcBootstrapVersion,
     hmrc                %% s"domain-$playVersion"                 % "9.0.0",
     hmrc                %% s"play-hal-$playVersion"               % "4.0.0",
@@ -22,8 +19,7 @@ object AppDependencies {
   def test(scope: String = "test, it"): Seq[ModuleID] = Seq(
     "org.scalatestplus"      %% "mockito-3-4"                     % "3.2.10.0"            % scope,
     "org.scalatestplus"      %% "scalacheck-1-17"                 % "3.2.17.0"            % scope,
-    "com.vladsch.flexmark"   % "flexmark-all"                     % "0.64.8"              % scope,
     "org.scalaj"             %% "scalaj-http"                     % "2.4.2"               % scope,
-    hmrc                     %% s"bootstrap-test-$playVersion"    % hmrcBootstrapVersion  % scope,
+    hmrc                     %% s"bootstrap-test-$playVersion"    % hmrcBootstrapVersion  % scope
   )
 }
